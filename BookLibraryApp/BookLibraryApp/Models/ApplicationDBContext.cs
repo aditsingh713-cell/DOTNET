9804e0a8d67bb6fc611f5800dd206781eaa5d78c
aditsingh713-cell <aditsingh713@gmail.com>
2026-05-19 14:47:51 -0400

    Book Library

---
+++ BookLibraryApp/BookLibraryApp/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookLibraryApp.Models;
+namespace BookLibraryApp.Models
+{
+    public class ApplicationDBContext: DbContext
+    {
+       
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
---
+++ BookLibraryApp/BookLibraryApp/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Display(Name = "Published Date")]
+        public DateTime PublishedDate { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
