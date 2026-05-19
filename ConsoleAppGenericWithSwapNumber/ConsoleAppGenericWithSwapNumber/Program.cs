3f0db526a96e9b1e9f58a8e3682d71d7ddeff8d0
aditsingh713-cell <aditsingh713@gmail.com>
2026-05-19 14:59:30 -0400

    Console App Generic With Swap Number and String

---
+++ ConsoleAppGenericWithSwapNumber/ConsoleAppGenericWithSwapNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleAppGenericWithSwapNumber
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //Number swap
+            int x = 5, y = 10;
+            Console.WriteLine($"Before swap: x = {x}, y = {y}");
+            Swap(ref x, ref y);
+            Console.WriteLine($"After swap: x = {x}, y = {y}");
+            //String swap
+            string str1 = "Hello", str2 = "World";
+            Console.WriteLine($"Before swap: str1 = {str1}, str2 = {str2}");
+            Swap(ref str1, ref str2);
+            Console.WriteLine($"After swap: str1 = {str1}, str2 = {str2}");
+            Console.ReadKey();
+        }
+    }
+}
